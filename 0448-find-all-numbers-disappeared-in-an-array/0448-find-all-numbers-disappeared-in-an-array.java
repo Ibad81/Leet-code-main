4fed04de2a865fa1b0d4166d70afe44b369fbc4e
Ibad <79755747+Ibad81@users.noreply.github.com>
2022-11-22 18:12:41 +0530

    Time: 19 ms (39.42%), Space: 51.6 MB (83.74%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,18 +1,18 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
         List<Integer> arr=new ArrayList<>();
-        for(int i=0;i<nums.length;i++){
-            int n=Math.abs(nums[i]);
-            if(nums[n-1]>0){
-            nums[n-1]=-1*nums[n-1];
-        }
-        }
-        for(int i=0;i<nums.length;i++){
-                if(nums[i]>0){
-                    arr.add(i+1);
-                }
+//         for(int i=0;i<nums.length;i++){
+//             int n=Math.abs(nums[i]);
+//             if(nums[n-1]>0){
+//             nums[n-1]=-1*nums[n-1];
+//         }
+//         }
+//         for(int i=0;i<nums.length;i++){
+//                 if(nums[i]>0){
+//                     arr.add(i+1);
+//                 }
                 
-            }
+//             }
         
         // for(int i=0;i<nums.length;i++){
         //     for(int j=0;j<nums.length;j++){
@@ -21,6 +21,16 @@ class Solution {
         //         }
         //     }
         // }
+        
+        HashSet<Integer> man=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            man.add(nums[i]);
+        }
+        for(int i=1;i<=nums.length;i++){
+            if(!man.contains(i)){
+            arr.add(i);
+            }
+        }
         return arr;
         
     }
