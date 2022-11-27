170750b3a04649d5f5185f81f9711676d751f670
Ibad <79755747+Ibad81@users.noreply.github.com>
2022-11-27 15:02:00 +0530

    Time: 1 ms (100.00%), Space: 44.5 MB (66.54%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.java
+++ 1528-shuffle-string/1528-shuffle-string.java
@@ -13,9 +13,12 @@ class Solution {
         for(int i=0;i<s.length();i++){
             arr[indices[i]]=s.charAt(i);
         }
-        for(int i=0;i<arr.length;i++){
-            str+=arr[i];
-        }
-        return str;
+        // for(int i=0;i<arr.length;i++){
+        //     str+=arr[i];
+        // }
+        String string = new String(arr);
+
+        // str=arr.toString();
+        return string;
     }
 }
