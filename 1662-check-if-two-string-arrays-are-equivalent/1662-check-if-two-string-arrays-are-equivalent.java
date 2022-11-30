0ad32b7f6ede78b3e948b33443754a58e056587d
Ibad <79755747+Ibad81@users.noreply.github.com>
2022-11-30 18:38:13 +0530

    Time: 2 ms (62.33%), Space: 41.6 MB (70.17%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -1,23 +1,18 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         
-        StringBuilder sb1 = new StringBuilder();
-        StringBuilder sb2 = new StringBuilder();
-        
-        boolean flag = false;
-        
-        for(String s1 : word1) {
-            sb1.append(s1);
+        String str="";
+        String str1="";
+        for(int i=0;i<word1.length;i++){
+            str+=word1[i];
+        }
+        for(int i=0;i<word2.length;i++){
+            str1+=word2[i];
         }
         
-        for(String s2 : word2) {
-            sb2.append(s2);
-        }
-        
-        if(sb1.toString().equals(sb2.toString())) {
+        if(str.equals(str1)){
             return true;
-        } else {
-            return false;
         }
+        return false;
     }
 }
