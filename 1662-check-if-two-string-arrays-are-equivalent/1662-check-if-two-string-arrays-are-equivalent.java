0cb13649a009bc2c3fb9cf27cfd79201df34ca3c
Ibad <79755747+Ibad81@users.noreply.github.com>
2022-11-30 18:37:51 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (94.01%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.java
@@ -1,18 +1,23 @@
 class Solution {
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         
-        String str="";
-        String str1="";
-        for(int i=0;i<word1.length;i++){
-            str+=word1[i];
-        }
-        for(int i=0;i<word2.length;i++){
-            str1+=word2[i];
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        
+        boolean flag = false;
+        
+        for(String s1 : word1) {
+            sb1.append(s1);
         }
         
-        if(str.equals(str1)){
-            return true;
+        for(String s2 : word2) {
+            sb2.append(s2);
         }
-        return false;
+        
+        if(sb1.toString().equals(sb2.toString())) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
